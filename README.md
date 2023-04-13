1bc2a58ea6587321d9e7b25647ae54b84e0b834d
Oscar <Romanoscar092@gmail.com>
2023-04-13 13:04:03 -0600

    pain

---
+++ README.md
@@ -0,0 +1,8 @@
+# WordGames
+#### CSIS 1400 FINAL - OSCAR ROMAN MARCIAL, NEPHI NORTON
+
+This repository was created for the final project of the
+Semester 2 2023 CSIS 1400 class.
+
+WordGames is our Java-based take on the popular games Wordle,
+Hangman, and Crossword.
---
+++ src/WordGames.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class WordGames {
+    public static final Scanner INPUT = new Scanner(System.in);
+    public static void main(String[] args) {
+        int selection;
+        do {
+            WordGames.printMenu();
+
+            try {
+                selection = WordGames.INPUT.nextInt();
+                WordGames.INPUT.nextLine();
+            } catch (Exception e) {
+                System.out.println("Error: Selection must be an integer ranging from 0-3. Exiting program!");
+                selection = 0;
+            }
+
+            switch (selection) {
+                case 0:
+                    System.out.println("Thanks for playing!");
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    HangmanGame hangman = new HangmanGame("Among");
+                    hangman.run();
+                    break;
+                case 3:
+                    break;
+            }
+        } while (selection != 0);
+    }
+
+    public static void printMenu() {
+        String menu = String.format("""
+                # .. Game (High Score)
+                
+                1 .. Wordle (%d)     2 .. Hangman (%d)
+                3 .. Crossword (%d)
+                                        
+                0 .. Exit
+                Your Selection:\s""", 0, HangmanGame.getHighScore(), 0); // Placeholder 0's
+        System.out.print(menu);
+    }
+}
